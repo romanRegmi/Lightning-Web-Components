b5d604750df80698b5faedd4161b37535bfb021e
Roman Regmi <romanregmi@hotmail.com>
2023-08-10 12:41:41 +0545

    Update weatherAPI.js

--- weatherAPI/weatherAPI.js
+++ weatherAPI/weatherAPI.js
@@ -23,35 +23,34 @@ export default class WeatherAPI extends LightningElement {
         try {
             const currentWeatherResponse = await getWeather({ city: this.cityName });
             let currentWeather = JSON.parse(currentWeatherResponse);
+            let current = currentWeather.current;
             
-            this.wind_mph = currentWeather.current.wind_mph;
-            this.temperature = currentWeather.current.temp_c;
-            this.humidity = currentWeather.current.humidity;
-            this.condition = currentWeather.current.condition.text;
-            this.imageURL = currentWeather.current.condition.icon;
-            
+            this.wind_mph = current.wind_mph;
+            this.temperature = current.temp_c;
+            this.humidity = current.humidity;
+            this.condition = current.condition.text;
+            this.imageURL = current.condition.icon;
             
             this.date = currentWeather.location.localtime.substring(0, 10);
             const t = new Date(currentWeather.location.localtime);
             this.time = t.toLocaleTimeString();
 
-
-
-
             const forecastWeatherResponse = await forecastWeather({ city: this.cityName });
             const hourlyWeather = forecastWeatherResponse.forecast.forecastday.at(0).hour;
 
             // clear the array
+            // otherwise it will hold the previous record.
             this.nextRecords = [];
 
             for (let i = 0; i < hourlyWeather.length; i++){
                 let nextHr = new Date(hourlyWeather.at(i).time_x);
-                if(nextHr > t){
-                    this.nextRecords.push(hourlyWeather.at(i))
-                }
-
+                
                 if(this.nextRecords.length == this.cards){
                     break;
+                }
+
+                if(nextHr > t){
+                    this.nextRecords.push(hourlyWeather.at(i))
                 }
             }
             
@@ -63,6 +62,7 @@ export default class WeatherAPI extends LightningElement {
     }
 
     handleCurrentLocation() {
-        
+        // will only change the cityName in UI.
+        this.cityName = 'London';
     }
 }
